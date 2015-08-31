8f14e7fa4bb08fc9c4696b48836d0b5650d9f075
Susan Lau <susan.lau@genj.nl>
2015-08-31 15:58:19 +0200

    add repository classes for ShortUrlBuilder in admin #8924

--- src/Genj/ShortUrlBundle/Entity/ShortUrlRepository.php
+++ src/Genj/ShortUrlBundle/Entity/ShortUrlRepository.php
@@ -33,4 +33,48 @@ class ShortUrlRepository extends EntityRepository
 
         return $query->getOneOrNullResult();
     }
+
+    /**
+     * @param string $target
+     *
+     * @return ShortUrl|null
+     *
+     * @throws \Doctrine\ORM\NonUniqueResultException
+     */
+    public function retrieveByTarget($target)
+    {
+        $query = $this->createQueryBuilder('s')
+            ->where('s.target like :target')
+            ->orderBy('s.publishAt', 'DESC')
+            ->setMaxResults(1)
+        ;
+
+        $query = $query->getQuery();
+
+        $query->setParameter('target', $target);
+
+        return $query->getOneOrNullResult();
+    }
+
+    /**
+     * @param string $source
+     *
+     * @return ShortUrl|null
+     *
+     * @throws \Doctrine\ORM\NonUniqueResultException
+     */
+    public function retrieveBySource($source)
+    {
+        $query = $this->createQueryBuilder('s')
+            ->where('s.source like :source')
+            ->orderBy('s.publishAt', 'DESC')
+            ->setMaxResults(1)
+        ;
+
+        $query = $query->getQuery();
+
+        $query->setParameter('source', $source);
+
+        return $query->getOneOrNullResult();
+    }
 }
